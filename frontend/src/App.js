8dcd61bd0297fa3ae5783bb965f903216deaed7e
chanis10 <isabellec.chan@mail.utoronto.ca>
2025-03-22 18:17:16 -0400

    Updates to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router components
+import Home from './Home'; // Import the Home component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Set the Home component as the default route */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
