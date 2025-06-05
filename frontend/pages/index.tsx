6a520984292bbe306f5a4d143d3ca8501342087d
wenjess1 <jessica.wen@mail.utoronto.ca>
2025-06-05 19:31:03 -0400

    front page stuff

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import background from '../src/images/background.jpg';
+import doorframe from '../src/images/doorframe.png';
+import leftdoor from '../src/images/leftdoor.png';
+import rightdoor from '../src/images/rightdoor.png';
 
 function Index() {
 
@@ -14,7 +19,42 @@ function Index() {
 
   return (
     <div>
-      {message}
+      <div className="relative z-20 text-black p-4">
+        {message}
+      </div>
+      <div className="absolute left-1/2 z-20"
+    style={{ top: '50%', transform: 'translateX(-50%) translateY(-25%)'}}>
+        <Image 
+          src={doorframe}
+          alt=""
+          width={350}
+          layout="intrinsic"
+          />
+      </div>
+      <div className="absolute left-1/2 z-20"
+    style={{ top: '50%', transform: 'translateX(0%) translateY(-12%)'}}>
+        <Image 
+          src={leftdoor}
+          alt=""
+          height={350}
+          layout="intrinsic"
+          />
+      </div>
+      <div className="absolute left-1/2 z-20"
+    style={{ top: '50%', transform: 'translateX(-102%) translateY(-12%)'}}>
+        <Image 
+          src={rightdoor}
+          alt=""
+          height={350}
+          layout="intrinsic"
+          />
+      </div>
+      <Image
+        src={background}
+        alt=""
+        layout="fill"
+        objectFit="cover"
+      />
     </div>
   );
 }
